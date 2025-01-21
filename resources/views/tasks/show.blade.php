33098491a7a9f4bb8d017de977704841bd2c698e
zonehisa <nijipapazoneokun123@gmail.com>
2025-01-21 13:19:04 +0900

    タスク管理アプリ課題完了

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,6 +8,7 @@
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <title>Document</title>
 </head>
+
 <body>
     <h1>タスク詳細</h1>
     <div class="group">
